869d58e6a740d0112bb0e614d6d039b14d5f4c84
KR <khyati@KRs-MacBook-Air.local>
2023-11-29 06:57:37 +0530

    Updates

---
+++ precache-manifest.58933e293d243dbef839ef71a8ab446a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c437162efbdb688e9a55eee3443f770",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "40e33213a44a6f7b6337",
+    "url": "/react-portfolio/static/css/main.aefb05ae.chunk.css"
+  },
+  {
+    "revision": "e19a9ba3cdf3c48d6e55",
+    "url": "/react-portfolio/static/js/2.c61f5966.chunk.js"
+  },
+  {
+    "revision": "40e33213a44a6f7b6337",
+    "url": "/react-portfolio/static/js/main.12f0804e.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "427cfc3c883f3d6ac33320e884194ece",
+    "url": "/react-portfolio/static/media/backg.427cfc3c.gif"
+  }
+]);
